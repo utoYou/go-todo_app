ca1ffd3af3df2dda766e5f0e14ed23edb8c1e257
utoYou <utoyou353@gmail.com>
2023-02-15 02:02:27 +0900

    userのモデルと追加処理を定義

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module todo_app
 go 1.19
 
 require (
+	github.com/google/uuid v1.3.0
 	github.com/mattn/go-sqlite3 v1.14.16
 	gopkg.in/go-ini/ini.v1 v1.67.0
 )
--- main.go
+++ main.go
@@ -14,4 +14,12 @@ func main() {
 	// log.Println("test")
 
 	fmt.Println(models.Db)
+
+	u := &models.User{}
+	u.Name = "test"
+	u.Email = "test@example.com"
+	u.Password = "testtest"
+	fmt.Println(u)
+
+	u.CreateUser()
 }
