3a9eaec2c3615a584056f0c8a00c7777a0248aa9
utoYou <utoyou353@gmail.com>
2023-02-19 02:27:12 +0900

    Todoデータを複数取得する処理を追加

--- main.go
+++ main.go
@@ -37,6 +37,29 @@ func main() {
 	// user, _ := models.GetUser(2)
 	// user.CreateTodo("First Todo")
 
-	t, _ := models.GetTodo(1)
-	fmt.Println(t)
+	// t, _ := models.GetTodo(1)
+	// fmt.Println(t)
+
+	u := &models.User{}
+	u.Name = "test2"
+	u.Email = "test2@example.com"
+	u.Password = "testtest"
+	fmt.Println(u)
+
+	u.CreateUser()
+
+	user, _ := models.GetUser(3)
+	user.CreateTodo("Third Todo")
+
+	// todos, _ := models.GetTodos()
+	// for _, v := range todos {
+	// 	fmt.Println(v)
+	// }
+
+	user2, _ := models.GetUser(2)
+	todos, _ := user2.GetTodosByUser()
+	for _, v := range todos {
+		fmt.Println(v)
+	}
+
 }
