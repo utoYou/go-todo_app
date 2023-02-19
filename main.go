dd6234949416ad08e42e0b4aa3c56ccbc3e831cc
utoYou <utoyou353@gmail.com>
2023-02-19 21:33:08 +0900

    サーバーを立ち上げる処理を追加&トップページ用のhtmlと処理追加

--- main.go
+++ main.go
@@ -1,66 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"todo_app/app/controllers"
 	"todo_app/app/models"
 )
 
 func main() {
-	// fmt.Println(config.Config.Port)
-	// fmt.Println(config.Config.SQLDriver)
-	// fmt.Println(config.Config.DbName)
-	// fmt.Println(config.Config.LogFile)
+	fmt.Println(models.Db)
 
-	// log.Println("test")
-
-	// fmt.Println(models.Db)
-
-	// u := &models.User{}
-	// u.Name = "test"
-	// u.Email = "test@example.com"
-	// u.Password = "testtest"
-	// fmt.Println(u)
-
-	// u.CreateUser()
-
-	// u, _ := models.GetUser(1)
-	// fmt.Println(u)
-
-	// u.Name = "Test2"
-	// u.Email = "test2@example.com"
-	// u.UpdateUser()
-	// u, _ = models.GetUser(1)
-	// fmt.Println(u)
-
-	// u.DeleteUser()
-
-	// user, _ := models.GetUser(2)
-	// user.CreateTodo("First Todo")
-
-	// t, _ := models.GetTodo(1)
-	// fmt.Println(t)
-
-	// u := &models.User{}
-	// u.Name = "test2"
-	// u.Email = "test2@example.com"
-	// u.Password = "testtest"
-	// fmt.Println(u)
-
-	// u.CreateUser()
-
-	// user, _ := models.GetUser(3)
-	// user.CreateTodo("Third Todo")
-
-	// todos, _ := models.GetTodos()
-	// for _, v := range todos {
-	// 	fmt.Println(v)
-	// }
-
-	// user2, _ := models.GetUser(2)
-	// todos, _ := user2.GetTodosByUser()
-	// for _, v := range todos {
-	// 	fmt.Println(v)
-	// }
-
-	t, _ := models.GetTodo(3)
-	t.DeleteTodo()
+	controllers.StartMainServer()
 }
